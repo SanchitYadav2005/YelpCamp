b1a9afd155ebf1f4a1b9fc21025fcf4acac513c5
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-09-24 18:37:43 +0530

    done with the day

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreteIndex: true,
+    
+})
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
