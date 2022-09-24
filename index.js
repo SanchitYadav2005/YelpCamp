71c4e501100eb8aad0f1b660e2c5979e820b25ea
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-09-24 16:46:58 +0530

    all things are working fine

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req,res)=>{
+    res.render('home')
+})
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(err);
+        console.log("error");
+    }else{
+        console.log("working");
+    }
+})
