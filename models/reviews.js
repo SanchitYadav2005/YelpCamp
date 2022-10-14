230a0ede29e76a6edbf7b6bdb6919733c0d2272e
unknown <sanchit2005yudavanshi@gmail.com>
2022-10-13 21:24:23 -0700

    added the review schema

---
+++ models/reviews.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const reviewSchema = new Schema({
+    body: String, // body represents review text.
+    rating: Number
+})
+
+module.exports = mongoose.model("Review", reviewSchema);
