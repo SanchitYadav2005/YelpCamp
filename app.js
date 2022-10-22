0286fbe9e554270eb58b1959557337c4c9865a02
unknown <sanchit2005yudavanshi@gmail.com>
2022-10-22 07:22:08 -0700

    added serving static files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override');
 
 const campground = require('./routes/campground');
 const review = require('./routes/review');
+const exp = require('constants');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -25,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // using the routes.
 app.use('/campgrounds', campground);
