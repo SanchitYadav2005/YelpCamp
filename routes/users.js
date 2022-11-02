0b0f1b3386e9c398ab9b8aa329983037a4d22624
unknown <sanchit2005yudavanshi@gmail.com>
2022-11-02 20:16:12 +0530

    added register post route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 
-router.get('/register', (req,res)=>{
+router.get('/register', (req, res) => {
     res.render('user/register');
 });
 
-router.post('/register', async (req,res)=>{
-    res.send(req.body);
+router.post('/register', async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'welcome to yelpcamp');
+        res.redirect('/campgrounds');
+    }catch(e){
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+
 })
 
 module.exports = router;
