4a74f21c31f29557fb4bd587f4ccdd47536edcf6
unknown <sanchit2005yudavanshi@gmail.com>
2022-11-04 21:15:30 +0530

    else is not working in navbar

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get('/register', (req, res) => {
     res.render('user/register');
 });
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         // getting email, username and password from requests body.
         const { email, username, password } = req.body;
@@ -18,35 +18,41 @@ router.post('/register', catchAsync(async (req, res) => {
         const user = new User({ email, username });
         // registering user with the .register method that is given by passport-local-mongoose that is in our shcema
         const registeredUser = await User.register(user, password);
-        // flashing success message if this works.
-        req.flash('success', 'welcome to yelpcamp');
-        // redirecting to campgrounds.
-        res.redirect('/campgrounds');
-    }catch(e){
+        // logining in the user after registering.
+        req.login(registeredUser, err => {
+            if (err) return next(err); 
+            // flashing success message if this works.
+            req.flash('success', 'welcome to yelpcamp');
+            // redirecting to campgrounds.
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
     }
 
 }));
 
-router.get('/login', (req,res)=>{
+router.get('/login', (req, res) => {
     res.render('user/login');
 });
 
-router.post('/login',passport.authenticate('local', {failureMessage:true, failureRedirect:'/login'}), (req,res)=>{
+router.post('/login', passport.authenticate('local', { failureMessage: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    res.redirect('/campgrounds');
-} );
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+});
 
-router.get('/logout', (req,res)=>{
-    req.logout((error)=>{
-        if(error){
+router.get('/logout', (req, res) => {
+    req.logout((error) => {
+        if (error) {
             return req.flash('error', error);
         }
         req.flash('success', 'Goodbye');
         res.redirect('/campgrounds');
     });
-    
+
 })
 
 module.exports = router;
