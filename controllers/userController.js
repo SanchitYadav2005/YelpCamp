812d7dbc6db1f3e8ee20a64d9fa4cff84300ec2b
unknown <sanchit2005yudavanshi@gmail.com>
2022-11-10 20:53:32 +0530

    added userController and replaced routes in it

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
+
+// route to get registration form.
+module.exports.getRegistrationPage = (req, res) => {
+    res.render('user/register');
+};
+
+// route to create user.
+module.exports.createUser = catchAsync(async (req, res, next) => {
+    try {
+        // getting email, username and password from requests body.
+        const { email, username, password } = req.body;
+        // specifying user.
+        const user = new User({ email, username });
+        // registering user with the .register method that is given by passport-local-mongoose that is in our shcema
+        const registeredUser = await User.register(user, password);
+        // logining in the user after registering.
+        req.login(registeredUser, err => {
+            if (err) return next(err); 
+            // flashing success message if this works.
+            req.flash('success', 'welcome to yelpcamp');
+            // redirecting to campgrounds.
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+
+});
+
+// route to get login form.
+
+module.exports.getLoginForm = (req, res) => {
+    res.render('user/login');
+}
+
+module.exports.loginUser = passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo:true }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+};
+
+module.exports.logoutUser = (req, res) => {
+    req.logout((error) => {
+        if (error) {
+            return req.flash('error', error);
+        }
+        req.flash('success', 'Goodbye');
+        res.redirect('/campgrounds');
+    });
+
+};
--- routes/users.js
+++ routes/users.js
@@ -1,57 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const catchAsync = require('../utils/catchAsync');
-const passport = require('passport');
+const userController = require('../controllers/userController');
 
 
-router.get('/register', (req, res) => {
-    res.render('user/register');
-});
+// route to get registration form.
+router.get('/register', userController.getRegistrationPage);
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        // getting email, username and password from requests body.
-        const { email, username, password } = req.body;
-        // specifying user.
-        const user = new User({ email, username });
-        // registering user with the .register method that is given by passport-local-mongoose that is in our shcema
-        const registeredUser = await User.register(user, password);
-        // logining in the user after registering.
-        req.login(registeredUser, err => {
-            if (err) return next(err); 
-            // flashing success message if this works.
-            req.flash('success', 'welcome to yelpcamp');
-            // redirecting to campgrounds.
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
+// route to create user.
+router.post('/register', userController.createUser);
 
-}));
+// route to get login form.
+router.get('/login', userController.getLoginForm);
 
-router.get('/login', (req, res) => {
-    res.render('user/login');
-});
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo:true }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-});
-
-router.get('/logout', (req, res) => {
-    req.logout((error) => {
-        if (error) {
-            return req.flash('error', error);
-        }
-        req.flash('success', 'Goodbye');
-        res.redirect('/campgrounds');
-    });
-
-})
+// route to login user.
+router.post('/login', userController.loginUser);
+// route to logout user.
+router.get('/logout', userController.logoutUser);
 
 module.exports = router;
