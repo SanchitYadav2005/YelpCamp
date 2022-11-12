174bb649bf44d8e183693e089ac2c65caa531986
unknown <sanchit2005yudavanshi@gmail.com>
2022-11-12 20:47:12 +0530

    .env is wokring fine

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+console.log(process.env.CLOUDINARY_KEY)
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
