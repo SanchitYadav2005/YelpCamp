d512f9619e7daa2d7dbde43c0e39da06168be2de
unknown <sanchit2005yudavanshi@gmail.com>
2022-11-19 18:44:11 +0530

    added mongo sanatization

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const reviewRoute = require('./routes/review');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -36,6 +37,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
